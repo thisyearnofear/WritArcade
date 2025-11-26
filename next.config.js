80217ec2e3252772809f2e31d27679b0c8722624
Quincybob <74205675+Quincybob@users.noreply.github.com>
2025-11-27 00:50:51 +0300

    Fix Turbopack bundling of test files from @wagmi/connectors

--- next.config.js
+++ next.config.js
@@ -16,6 +16,19 @@ const nextConfig = {
     // Enable strict type checking
     tsconfigPath: './tsconfig.json',
   },
+  webpack: (config, { isServer }) => {
+    // Exclude test files and unnecessary files from bundle
+    config.module.rules.push({
+      test: /node_modules\/@wagmi\/connectors\/node_modules\/thread-stream\/(test|bench)/,
+      use: 'null-loader'
+    });
+    return config;
+  },
+  experimental: {
+    turbotrace: {
+      logLevel: 'error'
+    }
+  }
 }
 
 module.exports = nextConfig
